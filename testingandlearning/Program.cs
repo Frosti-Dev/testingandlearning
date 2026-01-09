a0516f576e3bf5c0375a9aeb58b45a4ad6bbd4a4
Frosti-Dev <bennyfranken3@gmail.com>
2026-01-09 12:07:01 -0400

    classes/structs and properties

---
+++ testingandlearning/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace testingandlearning
+{
+    class Program
+    {
+        //structs and classes
+
+        //scrcut == value type
+        //class == reference type
+
+        //struct Point //could also be class. Functionally they are the same thing.
+        //{
+        //    //constructor
+        //    public Point(int startX, int startY)
+        //    {
+        //        _x = startX;
+        //        _y = startY;
+        //    }
+
+        //    int _x;
+
+        //    public int GetX()
+        //    {
+        //        return _x;
+        //    }
+
+        //    int _y;
+
+        //    public int GetY()
+        //    {
+        //        return _y;
+        //    }
+
+        //}
+
+        //class Rupee
+        //{
+        //    //constructor
+        //    //contructor is a special method that gets called when we instantiate a rupee.
+        //    public Rupee(Point position, ConsoleColor color, int moneyValue)
+        //    {
+        //        _position = position;
+        //        _color = color; 
+        //        _moneyValue = moneyValue;
+        //    }
+
+        //    Point _position;
+        //    public Point GetPosition()
+        //    {
+        //        return _position;
+        //    }
+
+        //    ConsoleColor _color;
+        //    public ConsoleColor GetColor()
+        //    {
+        //        return _color;
+        //    }
+
+        //    int _moneyValue;
+        //    public int GetMoneyValue()
+        //    {
+        //        return _moneyValue;
+        //    }
+        //}
+
+        static void Main(string[] args)
+        {
+            //Point p = new Point(10, 20);
+
+            //p._x = 100;
+            //p._y = 200;
+
+            //Point rupeeSpawnPos = new Point(startX: 5, startY: 5);
+            //Rupee myRupee = new Rupee(
+            //    position: rupeeSpawnPos, 
+            //    color: ConsoleColor.Green, 
+            //    moneyValue: 10
+            //    );
+            //do this if line is too long
+
+
+
+            //Console.BackgroundColor = myRupee.GetColor();
+
+            //Point rupeePoint = myRupee.GetPosition();
+            //Console.SetCursorPosition(rupeePoint.GetX(), rupeePoint.GetY());
+
+            //Console.Write(" ");
+            //Console.ReadKey();
+
+            //rectangle class in another file
+
+            Rectangle rect = new Rectangle();
+
+
+            float recWidth = rect.Width;
+
+            Console.ReadKey();      
+
+        }
+    }
+}
---
+++ testingandlearning/Rectangle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testingandlearning
+{
+    class Point
+    {
+        //constructor
+        public Point(int startX, int startY)
+        {
+            _x = startX;
+            _y = startY;
+        }
+
+        int _x;
+
+        public int GetX()
+        {
+            return _x;
+        }
+
+        int _y;
+
+        public int GetY()
+        {
+            return _y;
+        }
+    }
+
+    class Rectangle
+    {
+        // width
+        //private float _width;
+
+        //public float GetWidth()
+        //{
+        //    return _width;
+        //}
+
+        //public void SetWidth(float width)
+        //{
+        //    //check for illegal widths
+        //    _width = width;
+        //}
+
+        //property
+        public float Width { get;private set; } //does all the code below
+        //{
+        //    //old way
+
+            //get
+            //{
+            //    return _width;
+            //}
+            //set
+            //{
+            //    //if (value < 0)
+            //    //{
+            //    //    _width = value;
+            //    //}
+
+            //    //else
+            //    //{
+            //    //    _width = 0;
+
+            //    //    Console.ForegroundColor = ConsoleColor.Red;
+            //    //    Console.WriteLine("WARNING: TRIED RECT TO SET TO NEGATIVE NUMBER");
+            //    //    Console.ForegroundColor = ConsoleColor.White;
+            //    //}
+
+            //    _width = value;
+
+            //}
+
+            
+        //}
+
+
+        // height
+        //float _height; //gets added automatically
+        public float Height { get; private set; } //auto property
+
+        // position
+        Point _position;
+
+        // area
+        public float GetArea()
+        {
+            return Width * Height;
+        }
+
+        // perimeter
+        public float GetPerimeter()
+        {
+            return (Width * 2) + (Height * 2);
+        }
+
+        // corner points
+      
+    }
+}
